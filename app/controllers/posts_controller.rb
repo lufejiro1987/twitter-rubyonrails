734e5db27e6fa531f631c5f676ede030d5462db0
Luis Felipe Jimenez Roldan <luisfelipejimenezroldan@gmail.com>
2020-09-17 15:14:45 -0300

    avanzando en posts editar y borrar

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
     
     def index
-        @post = Post.all
+        @posts = Post.all
     end
     def show
         @post = Post.find(params[:id])
@@ -14,21 +14,30 @@ class PostsController < ApplicationController
     def create
         @post = Post.new(post_params)
         if @post.save
-            redirect_to root_path
+            redirect_to root_path, notice: 'has creado un nuevo post'
         else
-            redirect_to posts_path
+            redirect_to posts_path, alert: 'el post no ha sido creado'
         end
     end
 
-    # def create
-    #     byebug
-    #     Post.create(title: params[:title], content: params[:content], autor: params[:autor])
-    #     redirect_to root_path
-    # end
+    def edit
+        @post = Post.find(params[:id])
+    end
+    
+    def update
+        @post = Post.find(params[:id])
+        if @post.update(post_params)
+            redirect_to root_path, notice: 'actualizado correctamente'
+        else
+            redirect_to edit_post_path(params[:id]), alert: 'no se ha podido actualizar'
+        end
+    end
+
     def destroy
         @post = Post.find(params[:id])
+        @post_title = @post.title
         @post.destroy
-        redirecto_to root_path
+        redirect_to root_path, notice: "el post #{@post_title} ha sido eliminado"
     end
     
     private
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   # rutas collection
   # get, post/index
-  get 'posts/new', to: 'posts#new', as: 'posts'
+  get 'posts/new'
   post 'posts', to: 'posts#create' #las posts envian datos
-
+  
   #rutas member
-  get 'posts/:id', to: 'posts#show', as: 'show_post'
+  
+  get 'posts/:id', to: 'posts#show'
+  get 'posts/:id/edit', to: 'posts#edit', as: 'edit_post'
+  patch 'posts/:id', to: 'posts#update', as: 'post'
   delete 'posts/:id', to: 'posts#destroy', as: 'destroy_post'
 
   root to: 'posts#index'
